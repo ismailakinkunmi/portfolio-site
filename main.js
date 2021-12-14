173af29c86b4eb29281ef3e3c1b5abd7a3353d40
ahmed tawfik ali <kalkolty@hotmail.com>
2021-12-14 18:04:34 +0200

    Alter main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 function openNav() {
-  document.getElementById("myNav").style.height = "100%";
+  document.getElementById('myNav').style.height = '100%';
+}
+function closeNav() {
+  document.getElementById('myNav').style.height = '0';
 }
