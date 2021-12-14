133dd2b7af09f3e7e1ae30026adfb68d2f2b5529
Adekunle Ismail <adismail4@gmail.com>
2021-12-14 17:20:25 +0100

    Modify js file for errors report

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-function openNav() {
-  document.getElementById("myNav").style.height = "100%";
+function () {
+  document.getElementById('myNav').style.height = '100%';
 }
-function closeNav() {
-  document.getElementById("myNav").style.height = "0";
+function () {
+  document.getElementById('myNav').style.height = '0';
 }
