1e4063eac373eec205eeb956f6c31cd024fc07b9
Adekunle Ismail <adismail4@gmail.com>
2021-12-14 17:46:10 +0100

    Modify html files for errors report

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-function () {
-  document.getElementById('myNav').style.height = '100%';
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
 }
-function () {
-  document.getElementById('myNav').style.height = '0';
+function closeNav() {
+  document.getElementById("myNav").style.height = "0";
 }
