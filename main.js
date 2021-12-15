0f8c3d19ae601c9ddb5ab5dafc3e975349b14730
Adekunle Ismail <adismail4@gmail.com>
2021-12-15 11:47:33 +0100

    Add eslintrc.json and modify html and css files

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+// eslint-disable-next-line no-unused-vars
 function openNav() {
   document.getElementById('myNav').style.height = '100%';
 }
 
+/* Close */
+// eslint-disable-next-line no-unused-vars
 function closeNav() {
   document.getElementById('myNav').style.height = '0%';
 }
