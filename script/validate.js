37e6238cfffb3269cb3c585333976fac67758759
Adekunle Ismail <adismail4@gmail.com>
2021-12-18 16:18:37 +0100

    Add clientside validattion to the project

---
+++ script/validate.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('#contact form');
+const { email } = form.elements;
+
+function showMsg(input, msg) {
+  const small = document.createElement('small');
+  small.classList.add('error');
+  small.textContent = msg;
+  form.insertBefore(small, input);
+}
+
+function resetMsg() {
+  document.querySelectorAll('small').forEach((s) => s.remove());
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  resetMsg();
+  if (email.value !== email.value.toLowerCase()) {
+    showMsg(email, 'The email should be lowercase');
+    showMsg;
+    return false;
+  }
+  form.submit();
+  return true;
+});
