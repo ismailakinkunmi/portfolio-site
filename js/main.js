ecb4e670ca9d76e56f45b2a8fb9fea3a4dcc25d1
Adekunle Ismail <adismail4@gmail.com>
2021-12-21 12:13:49 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -1,20 +1,11 @@
 /* eslint-disable comma-dangle */
 /* eslint-disable indent */
 /* eslint-disable no-plusplus */
-const hamburger = document.querySelector('.menu');
-const links = document.querySelectorAll('.mobile-menu a');
-hamburger.addEventListener('click', () => {
-  if (hamburger.classList.contains('active')) {
-    hamburger.classList.remove('active');
-    document.querySelector('.mobile-menu').style.display = 'none';
-  } else {
-    hamburger.classList.add('active');
-    document.querySelector('.mobile-menu').style.display = 'block';
-  }
-});
+
 links.forEach((a) => {
   a.addEventListener('click', (e) => {
     e.preventDefault();
+
     hamburger.classList.remove('active');
     document.querySelector('.mobile-menu').style.display = 'none';
   });
