708aba63460d9c9454d2d1d94fd5cee408266667
unknown <butlermuwo@gmail.com>
2021-12-21 14:30:05 +0200

    Add to main.js file

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 /* eslint-disable comma-dangle */
 /* eslint-disable indent */
 /* eslint-disable no-plusplus */
+const hamburger = document.querySelector('.menu');
+const links = document.querySelectorAll('.mobile-menu a');
+hamburger.addEventListener('click', () => {
+  if (hamburger.classList.contains('active')) {
+    hamburger.classList.remove('active');
+    document.querySelector('.mobile-menu').style.display = 'none';
+  } else {
+    hamburger.classList.add('active');
+    document.querySelector('.mobile-menu').style.display = 'block';
+  }
+});
 
 links.forEach((a) => {
   a.addEventListener('click', (e) => {
