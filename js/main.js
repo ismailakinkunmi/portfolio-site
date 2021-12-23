1880696e6c491284d487807047c4576c88a16301
Adekunle Ismail <adismail4@gmail.com>
2021-12-24 00:00:18 +0100

    First commit

--- js/main.js
+++ js/main.js
@@ -189,3 +189,30 @@ document.querySelectorAll('.see-project').forEach((btn) => {
     });
   });
 });
+
+// validation
+const form = document.querySelector('#contact form');
+const { email } = form.elements;
+
+function showMsg(input, msg) {
+  const small = document.createElement('small');
+  small.classList.add('error');
+  small.textContent = msg;
+  form.insertBefore(small, input);
+}
+
+function resetMsg() {
+  document.querySelectorAll('small').forEach((s) => s.remove());
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  resetMsg();
+  if (email.value !== email.value.toLowerCase()) {
+    showMsg(email, 'Please enter your email in lowercase');
+    showMsg;
+    return false;
+  }
+  form.submit();
+  return true;
+});
